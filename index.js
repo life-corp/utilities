544fcf9403c674afdeec572a6e6df3c9617d4300
Seth Bonnie <sethkbonnie@gmail.com>
2017-10-30 16:38:05 -0500

    Expose mountShare, prompt, and walkSync in index

--- index.js
+++ index.js
@@ -2,4 +2,7 @@ module.exports = {
   copy: require('./src/copy'),
   mkdirPromise: require('./src/mkdir-promise'),
   mkdirs: require('./src/mkdirs'),
+  mountShare: require('./src/mount-share'),
+  prompt: require('./src/prompt'),
+  walkSync: require('./src/walk-sync'),
 };
