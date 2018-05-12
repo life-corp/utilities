1b19a0857495186d42b4bd5c59369776269cda15
Seth Bonnie <sethkbonnie@gmail.com>
2018-05-12 18:54:55 -0500

    Remove password requirement; add support for cifs on linux

--- src/mount-share.js
+++ src/mount-share.js
@@ -1,13 +1,25 @@
 const { exec } = require('child_process');
+const os = require('os');
 const mkdirp = require('./mkdir-promise');
 
 const fileExists = (err) => /file exists/i.test(err.message);
 
-const mountShare = ({ username, password, mountPath, sharePath }) =>
+const mountShare = ({ username, mountPath, sharePath }) =>
   new Promise((resolve, reject) => {
     mkdirp(mountPath)
       .then(() => {
-        const cmd = `mount_smbfs //${username}:${password}@${sharePath} ${mountPath}`;
+        let cmd;
+        switch(os.platform()) {
+          case 'darwin':
+            cmd = `mount_smbfs //lifecorp.net;${username}@${sharePath} ${mountPath}`;
+            break;
+          case 'linux':
+          //SERVER/sharename /mnt/mountpoint -o user=username,password=password,uid=username,gid=group,workgroup=workgroup,ip=serverip,iocharset=utf8
+            cmd = `mount -t cifs ${sharePath} ${mountPath} -o user=${username},iocharset=utf8`;
+            break;
+          default:
+            throw new Error('Expected platform to be one of darwin (macos) or linux got', os.platform());
+        }
         exec(cmd, (err, stdout, stderr) => {
           if (err) {
             if (fileExists(err)) {
